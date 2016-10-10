1a01bd4da711dd442577177b2f0c2f05b486b665
maxrake <maxrake@users.noreply.github.com>
2016-10-10 02:16:14 -0500

    re-factor level 2 solution

--- PythonChallenge/level_2.py
+++ PythonChallenge/level_2.py
@@ -1,12 +1,12 @@
 from collections import Counter
 
 
-def level_2(text, num_least_common):
-    """Find the rare characters in the input text string."""
-    return Counter(text).most_common()[:-num_least_common-1:-1]
+def level_2(text):
+    """Find the characters in the input text string that only appear once."""
+    count_dict = dict(Counter(text).most_common())
+    return ''.join(letter for letter in text if count_dict[letter] == 1)
 
 
 if __name__ == '__main__':
     with open(r'extras\level_2.txt') as f:
-        source = f.read().strip()
-        print(level_2(source, 10))
+        print(level_2(f.read()))
